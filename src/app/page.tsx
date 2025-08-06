f8ac45350725b39b6472d8c318c3ce91922a8d5e
Abid Noman <abidnoman@Abids-MacBook-Air.local>
2025-08-06 23:08:14 +0500

    live

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,122 @@
+"use client";
+
 import Image from "next/image";
+import * as motion from "motion/react-client";
+import { useMotionValue, useSpring } from "framer-motion";
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { ArrowDown, Code, SendHorizontal, Store } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const springConfig = { damping: 20, stiffness: 300 };
+  const x = useSpring(mouseX, springConfig);
+  const y = useSpring(mouseY, springConfig);
+
+  useEffect(() => {
+    const moveCursor = (e: MouseEvent) => {
+      mouseX.set(e.clientX - 16); // center offset
+      mouseY.set(e.clientY - 16);
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+    return () => window.removeEventListener("mousemove", moveCursor);
+  }, [mouseX, mouseY]);
+
+  const box = {
+    width: 100,
+    height: 100,
+    backgroundColor: "#ff0088",
+    borderRadius: 5,
+  };
+
+  return (
+    <section className="h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth">
+      <motion.div
+        className="fixed top-0 left-0 w-8 h-8 bg-white rounded-full pointer-events-none z-50 mix-blend-difference"
+        style={{ x, y }}
+      />
+      {/* Section 1 */}
+      <div className="h-screen snap-start flex flex-col items-center justify-center bg-black text-white gap-12">
+        <p className="relative">
+          <p className="text-2xl">Abid</p>
+          <p className="font-bold text-8xl text-orange-500">Noman</p>
+        </p>
+        <div className="grid grid-cols-4 grid-rows-2 w-full h-[400] gap-4">
+          <div className="flex flex-col justify-between col-span-2 row-span-2 bg-blue-700 w-full rounded-4xl relative overflow-hidden whitespace-nowrap">
+            <div className=""></div>
+            <motion.div
+              className="block text-white px-4 font-bold text-6xl font-mono"
+              style={{ x: 0 }}
+              animate={{ x: ["100%", "-100%"] }}
+              transition={{
+                repeat: Infinity,
+                duration: 10,
+                ease: "linear",
+              }}
+              whileHover={{ animationPlayState: "paused" }}
+            >
+              <span>
+                Fullstack Developer • React • Node.js • MongoDB • Tailwind •
+                Next.js • APIs • Stripe • Docker •
+              </span>
+            </motion.div>
+            <div className="flex justify-between items-center px-8 py-4">
+              <p className="font-mono font-bold">See all services</p>
+              <Button variant="link" className="text-white cursor-pointer">
+                <ArrowDown size={80} strokeWidth={2.75} />
+              </Button>
+            </div>
+          </div>
+          <div className="col-span-1 row-span-1 bg-white w-full rounded-4xl px-6 py-2">
+            <div className="flex w-full h-full flex-col-reverse justify-start">
+              <div className="flex justify-between items-center">
+                <p className="font-mono text-black font-bold">Work</p>
+                <Button variant="link" className="text-black cursor-pointer">
+                  <Code size={80} strokeWidth={2.75} />
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-1 row-span-1 bg-gray-800 w-full rounded-4xl px-6 py-2">
+            <div className="flex w-full h-full flex-col-reverse justify-start">
+              <div className="flex justify-between items-center">
+                <p className="font-mono text-white font-bold">About</p>
+                <Button variant="link" className="text-white cursor-pointer">
+                  <Store size={32} strokeWidth={2.75} className="font-bold"/>
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2 row-span-1 bg-yellow-300 w-full rounded-4xl px-6 py-2">
+            <div className="flex w-full h-full flex-col-reverse justify-start">
+              <div className="flex justify-between items-center">
+                <p className="font-mono text-black font-bold">Contact Us</p>
+                <Button variant="link" className="text-black cursor-pointer">
+                  <SendHorizontal size={48} strokeWidth={2.75} />
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+
+      {/* Section 2 */}
+      <div className="h-screen snap-start flex items-center justify-center bg-gray-900 text-white text-5xl">
+        Projects
+      </div>
+
+      {/* Section 3 */}
+      <div className="h-screen snap-start flex items-center justify-center bg-gray-800 text-white text-5xl">
+        About Me
+      </div>
+
+      {/* Section 4 */}
+      <div className="h-screen snap-start flex items-center justify-center bg-gray-700 text-white text-5xl">
+        Contact
+      </div>
+    </section>
   );
 }
